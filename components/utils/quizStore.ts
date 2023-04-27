8add4d701609e1a7e552e662225427e87239fb3d
Kireev Kirill <kireev.kd@perfema.com>
2023-04-28 02:51:57 +0300

    fix exports 2

--- components/utils/quizStore.ts
+++ components/utils/quizStore.ts
@@ -1,31 +1,33 @@
+import { Quiz } from "../types/quiz";
+
 export const getQuizes = () => {
-    const storage = localStorage.getItem("quizes")
-    if (storage) {
-        return JSON.parse(storage) as Quiz[];
-    }
-    return [] as Quiz[];
-}
+  const storage = localStorage.getItem("quizes");
+  if (storage) {
+    return JSON.parse(storage) as Quiz[];
+  }
+  return [] as Quiz[];
+};
 
 export const setQuizes = (v: Quiz[]) => {
-    localStorage.setItem("quizes", JSON.stringify(v));
-}
+  localStorage.setItem("quizes", JSON.stringify(v));
+};
 
 export const createQuiz = () => {
-    const quizes = getQuizes();
-    const id = quizes.reduce((maxId, v) => maxId > v.id ? maxId : v.id, 1) + 1;
-    quizes.push({
-        id: id,
-        title: `Квиз ${id}`,
-        description: "",
-        questions: [],
-    });
+  const quizes = getQuizes();
+  const id = quizes.reduce((maxId, v) => (maxId > v.id ? maxId : v.id), 1) + 1;
+  quizes.push({
+    id: id,
+    title: `Квиз ${id}`,
+    description: "",
+    questions: [],
+  });
 
-    setQuizes(quizes);
-    return id;
-}
+  setQuizes(quizes);
+  return id;
+};
 
 export const getMaxId = () => {
-    const quizes = getQuizes();
-    const id = quizes.reduce((maxId, v) => maxId > v.id ? maxId : v.id, 1);
-    return id;
-}
+  const quizes = getQuizes();
+  const id = quizes.reduce((maxId, v) => (maxId > v.id ? maxId : v.id), 1);
+  return id;
+};
