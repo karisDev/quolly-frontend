5477decd023225a746fa93d6064fceb411ab47b7
karisDev <karjaakirill@gmail.com>
2023-04-27 23:35:51 +0300

    new attempt to fix 404

--- app/constructor/[quiz_id]/page.tsx
+++ app/constructor/[quiz_id]/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 import cl from "./styles.module.scss";
 import { QuestionScreen } from "./screens";
 import SlidesScreen from "./screens/SlidesScreen";
 import FiltersScreen from "./screens/FiltersScreen";
 
-const Page = () => {
+const Page = ({ params }: { params: any }) => {
   return (
     <main style={{ display: "none" }} className={cl.newQuizPageWrapper}>
       <SlidesScreen />
